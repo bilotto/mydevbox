91533a941e2f0c75fb27b46fda82abb72b0ffed5
Fabio <fabio.bilotto@openet.com>
2021-07-21 19:16:23 -0300

    First commit

---
+++ basebox/provisioners/pip_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+sudo pip install --upgrade pip==20.1.1
+sudo pip install ansible==2.6.5.0
---
+++ basebox/provisioners/yum_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+# sudo yum -y update
+sudo yum -y install epel-release
+sudo yum -y install python-pip
+sudo yum -y install git
+# sudo yum -y install tigervnc-server
+sudo yum -y install xterm
+# sudo yum -y install openbox
+sudo yum -y install xorg-x11-fonts-misc
+sudo yum -y install xorg-x11-fonts-Type1
+sudo yum -y install wget
+sudo yum -y install net-tools
+sudo yum -y install vim
+sudo yum -y install dos2unix
+sudo yum -y install unzip  
+sudo yum -y install telnet 
+sudo yum -y install tcpdump
